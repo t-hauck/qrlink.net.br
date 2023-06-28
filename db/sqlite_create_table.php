2176a9ea0bcf5fc5fb491ae8b6fcd31c5d2d1393
Thiago Hauck <40044027+t-hauck@users.noreply.github.com>
2023-06-27 21:18:58 -0300

    Atualização do arquivo 'sqlite_create_table.php' conforme dados do 'qrlink.sql'

--- db/sqlite_create_table.php
+++ db/sqlite_create_table.php
@@ -8,12 +8,17 @@ if ($bd->exec($sql)) echo "\ntabela 'url_shorten' apagada\n";
 ////////
 $sql = "CREATE TABLE IF NOT EXISTS url_shorten (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            url text NOT NULL,
-            short_code varchar(50) NOT NULL,
-            short_code_password TEXT NULL;
-            hits int(11) NOT NULL,
-            last_acess timestamp NULL,
-            added_date timestamp NULL DEFAULT CURRENT_TIMESTAMP
+            original_url text NOT NULL,
+            short_code varchar(10) NOT NULL,
+            short_code_password TEXT NULL,
+
+            access int(11) NOT NULL DEFAULT 0,
+            last_access timestamp NULL,
+
+            access_attempts int(11) NULL,
+            last_access_attempt timestamp NULL,
+
+            added_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
             )
     ";
 if ($bd->exec($sql))
@@ -23,8 +28,8 @@ else
 
 ////////
 $sql = "
-        INSERT INTO url_shorten (url, short_code, hits) VALUES (
-        'http://127.0.0.1/link_de_teste', 'teste', '0')
+        INSERT INTO qrlink.url_shorten (original_url, short_code) VALUES (
+            'http://127.0.0.1/link_de_teste', 'teste')
     ";
 if ($bd->exec($sql)) 
     echo "\nLink inserido com sucesso, short_code: teste \n"; 
